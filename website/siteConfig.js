4407fa54f3f855876985ad8a049506b8d7a73d29
WakeTechBrandon <114441427+WakeTechBrandon@users.noreply.github.com>
2022-11-16 02:15:32 -0500

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -78,8 +78,8 @@ const siteConfig = {
     
     wrapPagesHTML: true,
     seperateCss: [
-        '${baseurl}pages/en/characters/overwatch/creed.html',
-        '${baseurl}pages/en/characters/overwatch/waylen.html',
+        '${baseurl}en/characters/overwatch/creed',
+        '${baseurl}en/characters/overwatch/waylen',
     ],
     
     stylesheets : [
